9cd06686b4798d2a09699105536051bc74c76fd7
Jiao Lin <linjiao@ornl.gov>
2016-11-08 11:05:14 -0500

    Refs #1. test_xs: added absorption

--- tests/test_xs.py
+++ tests/test_xs.py
@@ -30,18 +30,21 @@ def test_fccNi():
     lambdas = np.arange(0.05, 5.5, 0.001)
     T = 300
     calc = xscalc.XSCalculator(fccNi, T)
-    xs = [calc.xs_coh_el(l) for l in lambdas]
-    data = np.array([lambdas, xs])
+    coh_el_xs = [calc.xs_coh_el(l) for l in lambdas]
+    data = np.array([lambdas, coh_el_xs])
     expected = np.load('fccNi-coh-el-xs.npy')
     assert np.isclose(data, expected).all()
 
     inc_el_xs = [calc.xs_inc_el(l) for l in lambdas]
     inel_xs = [calc.xs_inel(l) for l in lambdas]
+    abs_xs = np.array([calc.xs_abs(l) for l in lambdas])
     if interactive:
         from matplotlib import pyplot as plt
-        plt.plot(lambdas, xs)
+        plt.plot(lambdas, coh_el_xs)
         plt.plot(lambdas, inc_el_xs)
         plt.plot(lambdas, inel_xs)
+        plt.plot(lambdas, abs_xs)
+        plt.plot(lambdas, abs_xs+coh_el_xs+inc_el_xs+inel_xs)
         plt.show()
     return
 
