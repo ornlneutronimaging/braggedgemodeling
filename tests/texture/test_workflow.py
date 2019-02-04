1bb90f3106cd8d482692f2d41bab9ed8bc477a6e
Jiao Lin <linjiao@ornl.gov>
2019-02-04 11:35:59 -0500

    Refs #22. test_workflow.py: added the step that uses mtex

--- tests/texture/test_workflow.py
+++ tests/texture/test_workflow.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
 
 import os
-from bem.texture.preparation.vdrive_handler import VDriveHandler
-from bem.texture.preparation.vdrive_to_mtex import VDriveToMtex
 
 here = os.path.dirname(__file__)
 
 def test():
+    # vdrive_filename.txt -> vulcan.rpf
     infile = os.path.join(here, '..', 'data', 'vdrive_filename.txt')
     interm = 'vulcan.intermediate'
-    outfile = 'vulcan.rpf'
+    rpffile = 'vulcan.rpf'
+    from bem.texture.preparation.vdrive_handler import VDriveHandler
+    from bem.texture.preparation.vdrive_to_mtex import VDriveToMtex
     o_vdrive = VDriveHandler(filename = infile)
     o_vdrive.run()
     o_vdrive.export(filename = interm)
 
     o_handler = VDriveToMtex(interm)
     o_handler.run()
-    o_handler.export(filename = outfile)
+    o_handler.export(filename = rpffile)
+
+    # vulcan.rpf -> vpsc.txt
+    vpscfile = 'vpsc.txt'
+    hkls = [[1,1,1], [2,0,0], [2,2,0], [2,2,2]]
+    from bem.texture import mtex
+    mtex.setup()
+    mtex.polfig2VPSC(rpffile, hkls, vpscfile)
     return
 
 
