983581d693936620c425940dec0c3fee31a9cd25
Jiao Lin <jiao.lin@gmail.com>
2022-02-03 18:08:53 -0800

    use "yaml.safe_load":   bem/__init__.py

--- bem/__init__.py
+++ bem/__init__.py
@@ -16,7 +16,7 @@ import yaml, os
 conf_path = "bem.conf"
 config = dict()
 if os.path.exists(conf_path):
-    config = yaml.load(open(conf_path))
+    config = yaml.safe_load(open(conf_path))
 
 from . import matter
 
